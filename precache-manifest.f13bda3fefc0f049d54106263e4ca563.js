3de639096f8ee790cd633782a314126101b15165
Steven Lim <limstevenlbw@gmail.com>
2019-11-20 14:31:24 -0800

    Updates

---
+++ precache-manifest.f13bda3fefc0f049d54106263e4ca563.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3226f5862332a463266f89ebb32bddc",
+    "url": "/ygo-draft-simulator/index.html"
+  },
+  {
+    "revision": "250b8ce32b9803ab3d48",
+    "url": "/ygo-draft-simulator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e48ec2b8eab8b4a70ea9",
+    "url": "/ygo-draft-simulator/static/js/2.5628898e.chunk.js"
+  },
+  {
+    "revision": "250b8ce32b9803ab3d48",
+    "url": "/ygo-draft-simulator/static/js/main.79d561b7.chunk.js"
+  },
+  {
+    "revision": "0eacffecf935de646d6e",
+    "url": "/ygo-draft-simulator/static/js/runtime-main.280c4b6e.js"
+  }
+]);
